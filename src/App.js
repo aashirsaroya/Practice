b14eb22edd1872e0baf16ec129cb8665dc5da186
Saroya <aashir.saroya@hpe.com>
2023-01-23 10:51:12 +0530

    Added a Dummy API to the React Login Page

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
   const Login = (details) => {
     console.log(details);
     if (
-      details.email == adminUser.email &&
-      details.password == adminUser.password
+      details.email === adminUser.email &&
+      details.password === adminUser.password
     ) {
       console.log("Logged In");
       setUser({
@@ -30,14 +30,33 @@ function App() {
   const Logout = () => {
     setUser({ name: "", email: "" });
   };
+
+  async function InvokeAPI() {
+    let response = await fetch("http://localhost:8000/message");
+    let data = await response.json();
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+    window.alert(data.message);
+  }
+
   return (
     <div className="App">
-      {user.email != "" ? (
+      {user.email !== "" ? (
         <div className="welcome">
           <h2>
             Welcome, <span>{user.name}</span>{" "}
           </h2>
           <button onClick={Logout}>Logout</button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button onClick={InvokeAPI}>Invoke API</button>
         </div>
       ) : (
         <LoginForm Login={Login} error={error} />
